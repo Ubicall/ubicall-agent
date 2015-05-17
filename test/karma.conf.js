b22ccc95e936eb5678c05b742fd28141e4aa85bc
waleed samy <waleed_samy@ymail.com>
2015-05-17 16:54:05 +0300

    add angular-css using bower install , because grunt wiredep will sync bower.json with index.html and remove unmatched

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -34,6 +34,7 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-local-storage/dist/angular-local-storage.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'bower_components/angular-css/angular-css.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
