186fb41dd4532e1f39f254a876e88daabdae0873
waleed samy <waleed_samy@ymail.com>
2015-05-26 12:34:48 +0300

    mock data from server

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,16 +7,20 @@
  * # MainCtrl
  * Controller of the agentUiApp
  */
- angular.module('agentUiApp')
- .controller('MainController', function ($scope ,$location, Auth , CallCenter) {
- 	if(!Auth.currentUser() || !Auth.currentUser().user){
- 		Auth.logout().then(function(){
- 			$location.path("/login");
- 		})
- 	}
- 	var user = Auth.currentUser().user;
- 	$scope.user= user;
- 	$scope.calls = CallCenter.getAvailablesCalls(user);
- 	$scope.queues = CallCenter.getQueues(user);
- 	
- });
+angular.module('agentUiApp')
+  .controller('MainController', function ($scope, $location, Auth, CallCenter) {
+    if (!Auth.currentUser() || !Auth.currentUser().user) {
+      Auth.logout().then(function () {
+        $location.path("/login");
+      })
+    }
+    var user = Auth.currentUser().user;
+    $scope.user = user;
+    $scope.calls = $scope.queues = [];
+    CallCenter.getAvailablesCalls().then(function (calls) {
+      $scope.calls = calls;
+    });
+    CallCenter.getQueues().then(function (queues) {
+      $scope.queues = queues;
+    });
+  });
--- app/scripts/services/callcenter.js
+++ app/scripts/services/callcenter.js
@@ -7,62 +7,42 @@
  * # CallCenter
  * Service in the agentUiApp.
  */
- angular.module('agentUiApp')
- .service('CallCenter', function () {
- 	var calls ;
- 	this.getAvailablesCalls = function(user) {
- 		calls=
- 		[{callid :1,queueid:1,name:'waleed',image:"images/home-pic-04.jpg",pigImage:"images/home-pic-01.jpg",title:'man we',fullName:'waleed samy',
- 		phone:'+201069527634',date:'2/1/2013',time:'7:38:05 AM'},
- 		{callid :2,queueid:2,name:'waleed',image:"images/home-pic-04.jpg",pigImage:"images/home-pic-01.jpg",title:'man we',fullName:'waleed samy',
- 		phone:'+201069527634',date:'2/1/2013',time:'7:38:05 AM'},
- 		{callid :3,queueid:3,name:'waleed',image:"images/home-pic-04.jpg",pigImage:"images/home-pic-01.jpg",title:'man we',fullName:'waleed samy',
- 		phone:'+201069527634',date:'2/1/2013',time:'7:38:05 AM'}
- 		];
+angular.module('agentUiApp')
+  .service('CallCenter', function ($http, $rootScope, $log, $q, localStorageService, AuthToken, API_BASE) {
+    var calls;
+    this.getAvailablesCalls = function () {
+      var deferred = $q.defer();
+      $http.get(API_BASE + "/calls").then(function (result) {
+        deferred.resolve(result.data);
+      }, function (error) {
+        deferred.reject(error);
+      });
+      return deferred.promise;
+    };
 
- 		return calls;
- 	};
+    this.getCallDetail = function (queueid, callid) {
+      var call = [];
+      angular.forEach(calls, function (item) {
+        if (item.callid === callid && item.queueid === queueid) {
+          this.push(item);
+        }
+      }, call);
 
- 	this.getCallDetail = function(queueid,callid){
- 		var call = [];
- 		angular.forEach(calls, function(item) {
- 			if(item.callid===callid && item.queueid===queueid){
- 				this.push(item);
- 			}
- 		}, call);
+      call = {
+        name: 'waleed', image: "images/home-pic-04.jpg",
+        pigImage: "images/home-pic-01.jpg", title: 'man we', fullName: 'waleed samy',
+        phone: '+201069527634', date: '2/1/2013', time: '7:38:05 AM'
+      };
+      return call;
+    };
 
- 		call ={name:'waleed',image:"images/home-pic-04.jpg",
- 		pigImage:"images/home-pic-01.jpg",title:'man we',fullName:'waleed samy',
- 		phone:'+201069527634',date:'2/1/2013',time:'7:38:05 AM'};
- 		return call;
- 	}
-
- 	this.getQueues = function(user){
- 		return [
- 		{
- 			"queueid" : 1 ,"name" :"Customer Support", 
- 			"calls" : 
- 			[
- 			{"callid" : 1 , "name" : "custo"},
- 			{"callid" : 2, "name" : "custome support 11"}
- 			]
- 		},
- 		{
- 			"queueid" : 2, "name" :"Red",
- 			"calls" : 
- 			[
- 			{"callid" : 1 , "name" : "Red 21"},
- 			{"callid" : 2, "name" : "Red 22"}
- 			]
- 		},
- 		{
- 			"queueid" : 3, "name":"New Customers", 
- 			"calls" : 
- 			[
- 			{"callid" : 1 , "name" : "New Customers 31"},
- 			{"callid" : 2, "name" : "New Customers 32"}
- 			]
- 		}
- 		];
- 	};
- });
+    this.getQueues = function () {
+      var deferred = $q.defer();
+      $http.get(API_BASE + "/queues").then(function (result) {
+        deferred.resolve(result.data);
+      }, function (error) {
+        deferred.reject(error);
+      });
+      return deferred.promise;
+    };
+  });
