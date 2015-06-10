0767d949d868f2c9c7c3205ef14f5b98240febbc
waleed samy <waleed_samy@ymail.com>
2015-06-10 17:12:26 +0300

    unstable version

--- app/scripts/controllers/phone.js
+++ app/scripts/controllers/phone.js
@@ -8,11 +8,14 @@
  * Controller of the agentUiApp
  */
 angular.module('agentUiApp')
-  .controller('PhoneController', function ($scope, FS_RTMP, alertService) {
+  .controller('PhoneController', function ($scope, $window, FS_RTMP, alertService) {
     var fsrtmp;
     var mics;
     var currentMic;
-    var status = "Waiting for communication server"
+    var calls = [];
+
+    $scope.status = "Waiting for communication server";
+    $scope.sessionid;
 
     $scope.flashvars = {
       rtmp_url: FS_RTMP
@@ -24,20 +27,84 @@ angular.module('agentUiApp')
 
     $scope.onLoadHandler = function (evt) {
       if (evt.success) {
-        mics = fsrtmp.micList();
-        currentMic = fsrtmp.getMic();
         fsrtmp = angular.element(document.querySelector("#" + evt.id))[0];
-        fsrtmp.login("1100@104.239.164.247", "26021234");
+
+        $window.onConnected = function (sessionid) {
+          $scope.sessionid = sessionid;
+          $scope.status = "Connected";
+          alertService.add("success", "rtmp session is " + sessionid);
+          $window.fsLogin("1112@104.239.164.247", "26021234");
+        };
+
+
+        $window.onDisconnected = function () {
+          $scope.status = "Disconnected";
+          $scope.sessionid = "";
+          setTimeout(function () {
+            $scope.status = "Connecting...";
+            fsrtmp.connect();
+          }, 5000);
+        };
+
+
+        $window.onLogin = function (status, user, domain) {
+          if (status != "success") {
+            alertService.add("danger", "Authentication failed! onAuth");
+          } else {
+            alertService.add("success", "connected to the matrix");
+            var u = user + '@' + domain;
+            fsrtmp.register(u, "");
+            // you logged in now with your sip credentials
+          }
+        };
+
+        $window.fsLogin = function (user, pass) {
+          fsrtmp.login(user, pass);
+        };
+
+        $window.fsLogout = function (account) {
+          fsrtmp.logout(account);
+        };
+
+        $window.fsHangup = function (uuid) {
+          fsrtmp.hangup(uuid);
+        };
+
+        $window.answer = function (uuid) {
+          fsrtmp.answer(uuid);
+        };
+
+        $scope.hangup = function () {
+          $window.fsHangup($scope.callUuid);
+          $scope.callIn = false;
+          $scope.callName = $scope.callNumber = $scope.callUuid = $scope.callAccount = '';
+          $scope.phone = "noCall";
+        };
+
+        $scope.answer = function () {
+          $window.fsHangup($scope.callUuid);
+          $scope.phone = "answer";
+        };
+
+        $window.onIncomingCall = function (uuid, name, number, account, evt) {
+          calls.push({uuid: uuid, name: name, number: number});
+          $scope.callName = name;
+          $scope.callNumber = number;
+          $scope.callUuid = uuid;
+          $scope.callAccount = account;
+          $scope.phone = "ringing";
+          $scope.callIn = true;
+
+          console.log(name + " " + number + " calling with uuid " + uuid);
+          $window.answer(uuid);
+        };
+
+        $window.onDebug = function (message) {
+          alertService.add("info", message);
+        }
+
       }
     };
 
-    $scope.onLogin = function (status, user, domain) {
-      if (status != "success") {
-        alertService.add("danger", "Authentication failed! onAuth");
-      } else {
-        var u = user + '@' + domain;
-        fsrtmp.register(u, "");
-        // you logged in now with your sip credentials
-      }
-    }
+
   });
