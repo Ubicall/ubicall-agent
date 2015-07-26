ac2cad19d3c95f8cf0930099c340010b02cc2695
waleed samy <waleed_samy@ymail.com>
2015-07-26 15:09:42 +0300

    use version v1 of api

--- app/app.js
+++ app/app.js
@@ -25,7 +25,7 @@ angular
     'swfobject'
   ]);
 angular
-  .module('agentUiApp').constant('API_BASE', 'https://agent.ubicall.com/api'); // TODO : standardized url for api in dev and prod (may use config file)
+  .module('agentUiApp').constant('API_BASE', 'https://agent.ubicall.com/api/v1'); // TODO : standardized url for api in dev and prod (may use config file)
 angular
   .module('agentUiApp').constant('FLASH_OBJ_VARS', {rtmp_url : 'rtmp://104.239.164.247/phone'});
 angular
